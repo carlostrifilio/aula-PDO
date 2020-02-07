9177696962eca0e892be96811b9b727625cda85b
carlostrifilio <chtrifilio@gmail.com>
2020-02-07 09:57:32 -0300

    atualizaçao pagina cadastro

--- cadastroLivro.php
+++ cadastroLivro.php
@@ -1,61 +1,65 @@
 <?php
-require_once ('conexao.php');
-
+require_once('conexao.php');
 $consulta = $conexaoDB->prepare('SELECT * from editora');
 $resultado = $consulta->execute();
 
 $editoras = $consulta->fetchAll(PDO::FETCH_ASSOC);
+
 if (isset($_POST['cadastro-livro'])) {
 
-    // verifica campos preenchidos
-    if ($_POST['nome'] != "" && $_POST['descricao'] != ""){
+  //verifica campos preenchidos
+  if ($_POST['nome'] != "" && $_POST['descricao'] != "") {
 
-// prepara a query
-$query = $conexaoDB->prepare('INSERT INTO livraria (nome,descricao,preco,fk_editora,fk_categoria,imagem) VALUES (:nome, :descricao, :preco, :fk_editora,49,sem-imagem)');
+    //prepara a query
+    $query = $conexaoDB->prepare('INSERT INTO produto (nome, descricao, preco, fk_editora, fk_categoria, imagem) VALUES (:nome, :descricao, :preco, :fk_editora, 1, "sem-imagem")');
 
-$resultado = $query->execute([
-    ":nome"=>$_POST['nome'],
-    ":descricao"=>$_POST['descricao'],
-    ":preco"=>$_POST['preco'],
-    ":fk_editora"=>$_POST[fk_editora],
+    $resultado = $query->execute([
+      ":nome" => $_POST['nome'],
+      ":descricao" => $_POST['descricao'],
+      ":preco" => $_POST['preco'],
+      ":fk_editora" => $_POST['fk_editora']
     ]);
 
-    // se tudo der certo, redireciona para lista de livros
-header('location: livros.php');
-    }
+    //se tudo der certo, redireciona para lista de livros
+    header('location: livros.php');
+  }
 }
 
-
 ?>
+
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Cadastro</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <title>Cadastro</title>
 </head>
+
 <body>
-    <form action="" method="post" class"container">
-        <label for="nome">Nome do Produto</label>
-            <input type="text" name="nome" id="nome" class="form-control"><br>
-        <label for="descricao">Descrição</label>
-            <input type="text" name="descricao" id="descricao" class="form-control"><br>
-        <label for="preco">Preço </label>
-            <input type="number" name="preco" class="form-control"><br>
-        <label for="">Imagem</label>
-            <input type="file" name="imagem" class="form-control"><br>
-        <label for="fk_editora" id="fk_editora">Editora</label>
-        <select name="fk_editora" id="fk_editora" class="form-control">
-        <?php foreach ($editoras as $editora) :?>
-            <option value=""> <?php echo $editora['nome']; ?></option>            
-<?php endforeach; ?>
-        ?>
-        </select>
-        <button name="cadastro-livro" class="btn btn-primary">Enviar</button>
-
-    </form>
-
+  <div class="container my-5">
+    <h1>Cadastrar livro</h1>
+  </div>
+  <form action="" method="POST" class="container">
+    <label for="nome">Nome Produto:</label>
+    <input type="text" id="nome" name="nome" class="form-control">
+    <label for="descricao">Descrição:</label>
+    <input type="text" name="descricao" id="descricao" class="form-control">
+    <label for="preco">Preço:</label>
+    <input type="number" name="preco" id="preco" class="form-control">
+    <label for="imagem">Imagem:</label>
+    <input type="file" name="imagem" id="imagem" class="form-control">
+    <label for="fk_editora">Editora</label>
+    <select name="fk_editora" id="fk_editora" class="form-control">
+      <?php foreach ($editoras as $editora) { ?>
+        <option value="<?php echo $editora["id_editora"]; ?>"> <?php echo $editora["nome"]; ?></option>
+      <?php } ?>
+    </select>
+    <button name="cadastro-livro" class="btn btn-primary"> Enviar </button>
+  </form>
 </body>
+
 </html>
--- conexao.php
+++ conexao.php
@@ -1,14 +1,19 @@
-<?php
+<?php 
 
-$dsn = "mysql:host=localhost;dbname=livraria;port=3307;charset=utf8mb4";
+$dsn = "mysql:host=localhost;dbname=livraria;port=3307";
 $username = "root";
 $password = "";
 
 try {
-   $conexaoDB = new PDO($dsn, $username, $password);
- // echo "<h1> DEU CERTO MEU IRMÃO</h1>";
-} catch(PDOException $e){
-    echo "<h1> Nessa máquina não funciona!</h1><br>".$e->getMessage();
+  $conexaoDB = new PDO($dsn, $username, $password);
+ // echo "<h1>DEU CERTO MEU IRMÃO</h1>";
+} catch (PDOException $e){
+ echo "<h1> Nessa máquina não funciona!</h1><br>".$e->getMessage();
 }
 
-?>
+
+
+
+
+
+?> 
